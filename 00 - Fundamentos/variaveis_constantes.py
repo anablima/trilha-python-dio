553290683fdc8c6230536437ad5e77d3e46949d7
Ana Beatriz Ferreira Lima <Ana.Beatriz.Lima@ibm.com>
2025-10-10 18:20:35 -0300

    Vídeo: Variáveis e constantes

--- 00 - Fundamentos/variaveis_constantes.py
+++ 00 - Fundamentos/variaveis_constantes.py
@@ -7,6 +7,7 @@ print(nome, idade)
 
 limite_saque_diario = 1000
 
+# Exemplo de sinalização de constantes, valores que não devem ser alterados
 BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
 
 print(BRAZILIAN_STATES)
