6e0afcc876f5e0540538d619ec4ba14ac8c19f98
Ana Beatriz Lima <ana.bea.f.lima@gmail.com>
2025-10-11 08:04:36 -0300

    Vídeo: Operadores de atribuição

--- 00 - Fundamentos/operadores_atribuicao.py
+++ 00 - Fundamentos/operadores_atribuicao.py
@@ -1,26 +1,34 @@
 saldo = 500
 print(saldo)
 
+# Atribuição simples
 saldo = 200
 print(saldo)
 
+# Adição no valor já existente na variável
 saldo += 10
 print(saldo)
 
+# Subtração no valor já existente na variável
 saldo -= 5
 print(saldo)
 
+# Divisão inteiro com o valor já existente na variável
 saldo //= 2
 print(saldo)
 
+# Dividindo o valor já existente na variável
 saldo /= 2
 print(saldo)
 
+# Multiplicando o valor já existente na variável
 saldo *= 10
 print(saldo)
 
+# Resto da divisão do valor já existente na variável
 saldo %= 4
 print(saldo)
 
+# Elevando o valor já existente na variável
 saldo **= 2
 print(saldo)
