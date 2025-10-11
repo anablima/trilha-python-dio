deea0d0d97034d9ab04dc3787bdcab19d7cb8996
Ana Beatriz Lima <ana.bea.f.lima@gmail.com>
2025-10-11 10:14:53 -0300

    Vídeo: Indentação e blocos + Nova organização de Exemplos em Fundamentos

--- 00 - Fundamentos/identacao_blocos.py
+++ 00 - Fundamentos/identacao_blocos.py
@@ -1,12 +1,12 @@
-def sacar(valor):
+def sacar(valor): # início do bloco do método
     saldo = 500
 
-    if saldo >= valor:
+    if saldo >= valor: # início do bloco do if
         print("valor sacado!")
         print("retire o seu dinheiro na boca do caixa.")
-
+    # fim do bloco do if
     print("Obrigado por ser nosso cliente, tenha um bom dia!")
-
+# fim do bloco do método
 
 def depositar(valor):
     saldo = 500
